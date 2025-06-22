187e215c326abd28176a75e9e362f9532f412a70
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-22 23:09:26 +0545

    Learning about import and export patterns in ES modules

---
+++ NodeJs/Day22/main.mjs
@@ -0,0 +1,26 @@
+//For 1st and 2nd patterns
+// import add from "./math-esm.mjs";
+
+// console.log(add(5,5));
+
+//For 3rd patter
+// import math from "./math-esm.mjs";
+
+// // console.log(math.add(5,5));
+// // console.log(math.subtract(5,5));
+
+// //destructing function from the object
+// const {add, subtract} = math;
+// console.log(add(5,5));
+// console.log(subtract(5,5));
+
+//For 4th pattern
+// import * as math from "./math-esm.mjs";
+
+// const {add, subtract} = math;
+// console.log(add(5,5));
+// console.log(subtract(5,5));
+
+import {add, subtract} from "./math-esm.mjs";
+console.log(add(5,5));
+console.log(subtract(5,5));
