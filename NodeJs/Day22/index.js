b5e1b8900231ef5ba98ff4c1b77e5c306c1acb08
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-22 23:08:40 +0545

    Learning about Fetching data from JSON

---
+++ NodeJs/Day22/index.js
@@ -0,0 +1,13 @@
+// const math = require("./math");
+
+// const {add, subtract} = math; //ES6 destructing
+
+// console.log(add(2,3));
+// console.log(subtract(2,3));
+
+const data = require("./data.json");
+console.log(data);
+console.log(data.name);
+console.log(data.address);
+
+//use node --watch filename for watchmode to monitor changes in the file
