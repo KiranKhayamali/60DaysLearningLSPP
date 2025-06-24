c0dc4ea7ce69f01ed92aedf079d7b9eeec309ff7
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-24 23:35:36 +0545

    Learning about pipes

--- NodeJs/Day24/stream.js
+++ NodeJs/Day24/stream.js
@@ -1,13 +1,21 @@
 const fs = require("node:fs");
+const zlib = require("node:zlib"); //helps in compressing in zip
+
+const gzip = zlib.createGzip()
 
 const readableStream = fs.createReadStream("./test.txt", {
     encoding: "utf-8", 
     highWaterMark: 2, //2 bytes will be the chunk size
 });
 
+readableStream.pipe(gzip).pipe(fs.WriteStream("./test2.txt.gz"))
+
 const writeableStream = fs.createWriteStream("./test2.txt");
 
-readableStream.on("data", (chunk) => {
-    console.log(chunk);
-    writeableStream.write(chunk);
-});
+// readableStream.on("data", (chunk) => {
+//     console.log(chunk);
+//     writeableStream.write(chunk);
+// });
+
+//Pipes
+readableStream.pipe(writeableStream);
