81984fac985293b3af14c8e62386ea7efb8b9613
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-26 23:36:43 +0545

    Learning about thread pool in asynchronous hashing

--- NodeJs/Day26/thread-pool.js
+++ NodeJs/Day26/thread-pool.js
@@ -1,8 +1,18 @@
 const crypto = require("node:crypto");
 
 //synchronous hashing
+// const start = Date.now();
+// crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
+// crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
+// crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
+// console.log("Hash: ", Date.now() - start);
+
+//asynchronous hashing
+const MAX_CALLS = 5; //libuv's thread pool has 4 threads
+
 const start = Date.now();
-crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
-crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
-crypto.pbkdf2Sync("password", "salt",100000, 512, "sha512");
-console.log("Hash: ", Date.now() - start);
+for(let i = 0; i<MAX_CALLS; i++) {
+    crypto.pbkdf2("password", "salt",100000, 512, "sha512", () => {
+        console.log(`Hash: ${i + 1}`, Date.now() - start);
+    });
+}
