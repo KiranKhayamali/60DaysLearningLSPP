8e4c418748580f5792e75d143e2fe963cf0184ad
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-28 00:04:14 +0545

    Learning about close queue in Event Loop

---
+++ NodeJs/Day27/close_queue.js
@@ -0,0 +1,12 @@
+const fs = require("node:fs");
+
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+readableStream.on("close", () => {
+    console.log("This is from readableStream close event callback");
+});
+setImmediate(() => console.log("This is setImmediate 1"));
+setTimeout(() => console.log("This is setTimeout 1"));
+process.nextTick(() => console.log("This is process nextTick 1 "));
+Promise.resolve().then(() => console.log("This is Promise resolve 1"));
