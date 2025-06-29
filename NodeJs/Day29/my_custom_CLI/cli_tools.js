503a8ebe232127740051eb2641c174c82e7094f2
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-29 23:17:51 +0545

    Integrating interactivity from the user

--- NodeJs/Day29/my_custom_CLI/cli_tools.js
+++ NodeJs/Day29/my_custom_CLI/cli_tools.js
@@ -2,8 +2,10 @@
 
 // console.log("Learning about pokedex");
 
-const yargs = require("yargs");
-const {argv} = yargs(process.argv);
+// const yargs = require("yargs");
+// const {argv} = yargs(process.argv);
+
+const inquirer = require("inquirer");
 const printTenMoves = async (pokemonName) => {
     const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
@@ -13,4 +15,13 @@ const printTenMoves = async (pokemonName) => {
     console.log(moves.slice(0,10));
 };
 
-printTenMoves(argv.pokemon);
+const prompt = inquirer.createPromptModule()
+prompt([{
+    type: "input",
+    name: "pokemon",
+    message: "Enter a pokemon name to view its first 10 moves:",
+    },
+]).then((answers) => {
+    const pokemon = answers.pokemon;
+    printTenMoves(pokemon);
+})
