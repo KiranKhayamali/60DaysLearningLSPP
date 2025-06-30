efc3d1c029b4138439b9d5c8f992ca428b1bf6a7
Kiran Khayamali <kirankhayamali@gmail.com>
2025-06-30 19:57:42 +0545

    Implementing Worker Thread Module for better performance

--- NodeJs/Day30/main_thread.js
+++ NodeJs/Day30/main_thread.js
@@ -1,16 +1,16 @@
 const http = require("node:http");
+const {Worker} = require("node:worker_threads");
 
 const server = http.createServer((req, res) => {
     if(req.url === "/") {
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.end("Home page");
     } else if (req.url === "/slow-page") {
-        let j =0;
-        for(let i = 0; i<4000000000; i ++) {
-            j++;
-        } 
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(`Slow page ${j}`);
+        const worker = new Worker("./worker_thread.js");
+        worker.on("message", (j) => {
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end(`Slow page ${j}`);
+        });
     }
 });
 
