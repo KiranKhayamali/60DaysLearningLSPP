885ff993160ed4d25fabf8f40b4c7ebec66e8160
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-01 17:38:21 +0545

    Making mockUser variable

--- ExpressJs/Day31/src/index.mjs
+++ ExpressJs/Day31/src/index.mjs
@@ -1,8 +1,17 @@
-import express, { request, response } from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const mockUsers = [
+    {id: 1, username: "kiran", displayName: "Kiran"},
+    {id: 2, username: "ashura", displayName: "Ashura"},
+    {id: 3, username: "unknown", displayName: "Unknown"},
+    {id: 4, username: "ded", displayName: "Dead"},
+    {id: 5, username: "rere", displayName: "Rere"},
+    {id: 6, username: "otokodes", displayName: "Aadim"},
+    {id: 7, username: "yog-anish", displayName: "Anish"},
+];
 
 app.listen(PORT, () => { //localhost:3000
     console.log(`Server running on port ${PORT}`);
@@ -12,18 +21,6 @@ app.listen(PORT, () => { //localhost:3000
 app.get("/", (request, response) => {
     response.status(201).send({msg : "Hello World!"});
 });
-
-app.get("/api/users", (request, response) => {
-    response.send([
-        {id: 1, username: "kiran", displayName: "Kiran"},
-        {id: 2, username: "ashura", displayName: "Ashura"},
-        {id: 3, username: "unknown", displayName: "Unknown"},
-        {id: 4, username: "ded", displayName: "Dead"},
-        {id: 5, username: "rere", displayName: "Rere"},
-        {id: 6, username: "otokodes", displayName: "Aadim"},
-        {id: 7, username: "yog-anish", displayName: "Anish"},
-    ]);
-}); 
 
 app.get("/api/products", (request, response) => {
     response.send([
