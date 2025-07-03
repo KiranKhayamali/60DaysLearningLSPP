302bbbf5bf6cdbc978afd046ec31259a22b87000
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-03 16:22:02 +0545

    Grouping all the validation method used in a custom schema

---
+++ ExpressJs/Day31/src/utils/validationSchema.mjs
@@ -0,0 +1,20 @@
+export const createUserValidationSchema = {
+    username : {
+        isLength : {
+            options : {
+                min : 3,
+                max : 32,
+            },
+            errorMessage : "Username must be at least 3 and at most 32 characters",
+        },
+        notEmpty : {
+            errorMessage : "Username cannot be empty",
+        },
+        isString : {
+            errorMessage : "Username must be a string!",
+        }, 
+    },  
+    displayName : {
+        notEmpty : true,
+    },
+}
