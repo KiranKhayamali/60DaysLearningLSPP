d3e0cd318d683c0ca9617e73abbd7eb1cf62e93a
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-04 18:13:01 +0545

    Implementing cookies for accessing products

--- ExpressJs/Day31/src/routes/products.mjs
+++ ExpressJs/Day31/src/routes/products.mjs
@@ -1,11 +1,17 @@
-import { Router } from "express";
+import { raw, Router } from "express";
 
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-    response.send([
-        {id: 123, name: "Iphone 16", price: "not afordable"}
-    ])
+    console.log(request.headers.cookie);
+    console.log(request.cookies);
+    console.log(request.signedCookies.hello);
+    if (request.cookies.hello && request.cookies.hello === "")
+        return response.send([
+            {id: 123, name: "Iphone 16", price: "not afordable"}
+        ]);
+
+    return response.status(403).send({msg: "Sorry, You need the correct cookies"});
 });
 
 
