79b2bd9e133537cf980f5ab5d408c697dbeb70ea
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-04 17:38:09 +0545

    Minimizing usage of base index using router

--- ExpressJs/Day31/src/index.mjs
+++ ExpressJs/Day31/src/index.mjs
@@ -1,45 +1,12 @@
 import express from "express";
-import {body, query, validationResult, matchedData, checkSchema} from "express-validator";
-import { createUserValidationSchema } from "./utils/validationSchema.mjs";
-import { createUserQueryValidationSchema } from "./utils/queryValidationSchema.mjs";
-
+import routes from "./routes/index.mjs"
 
 const app = express();
 
 app.use(express.json()); //middleware
-
-const loggingMiddleWare = (request, response, next) => {
-    console.log(`${request.method} - ${request.url}`);
-    next();
-};
-
-// app.use(loggingMiddleWare, (request, response, next) => { //enabling middleware globally and we can implement multiple middleware
-//     console.log("Finished Logging......");
-//     next();
-// }); 
-
-const resolveIndexByUserId = (request, response, next) => { //Similar code from put, patch and delete request are overrid by middleware
-    const { params: {id} } = request;
-
-    const parseID = parseInt(id);
-    if (isNaN(parseID)) return response.sendStatus(400); //Invalid id
-
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parseID);
-    if (findUserIndex === -1) return response.sendStatus(404); 
-    request.findUserIndex = findUserIndex;
-    next(); 
-};
+app.use(routes);
 
 const PORT = process.env.PORT || 3000;
-const mockUsers = [
-    {id: 1, username: "kiran", displayName: "Kiran"},
-    {id: 2, username: "ashura", displayName: "Ashura"},
-    {id: 3, username: "unknown", displayName: "Unknown"},
-    {id: 4, username: "ded", displayName: "Dead"},
-    {id: 5, username: "rere", displayName: "Rere"},
-    {id: 6, username: "otokodes", displayName: "Aadim"},
-    {id: 7, username: "yog-anish", displayName: "Anish"},
-];
 
 app.listen(PORT, () => { //localhost:3000
     console.log(`Server running on port ${PORT}`);
@@ -57,72 +24,6 @@ app.get("/", (request, response, next) => {
     next();
 }, (request, response) => {
     response.status(201).send({msg : "Hello World!"});
-});
-
-app.get("/api/products", (request, response) => {
-    response.send([
-        {id: 123, name: "Iphone 16", price: "not afordable"}
-    ])
-});
-
-//Query parameters
-app.get("/api/users", checkSchema(createUserQueryValidationSchema), (request, response) => {
-    const result = validationResult(request);
-    console.log(result);
-    const {query: {filter, value},} = request;
-    //When filter and value are undefined
-    // if (!filter && !value) return response.send(mockUsers);
-    if (filter && value) return response.send(
-        mockUsers.filter((user) => user[filter].includes(value))
-    );
-    
-    return response.send(mockUsers);
-});
-
-//POST requests
-app.post("/api/users", checkSchema(createUserValidationSchema), (request, response) => {
-    const result = validationResult(request);
-    console.log(result);
-
-    if (!result.isEmpty()) {
-        return response.status(400).send({errors: result.array()});
-    };
-    
-    const data = matchedData(request);
-    console.log(data); 
-    const newUser = { id: mockUsers[mockUsers.length -1].id +1, ...data };
-    mockUsers.push(newUser);
-    return response.status(201).send(newUser);
-});
-
-//PUT requests
-app.put("/api/users/:id",resolveIndexByUserId, (request, response) => {
-    const {body, findUserIndex} = request;
-
-    mockUsers[findUserIndex] = {id: mockUsers[findUserIndex].id, ...body};
-    return response.sendStatus(200);
-});
-
-//PATCH requests
-app.patch("/api/users/:id", resolveIndexByUserId, (request, response) => {
-    const {body, findUserIndex, } = request;
-    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body};
-    return response.sendStatus(200);
-});
-
-//DELETE requests
-app.delete("/api/users/:id", resolveIndexByUserId, (request, response) => {
-    const {findUserIndex} = request;
-    mockUsers.splice(findUserIndex, 1); //delete the item
-    return response.sendStatus(200);
-});
-
-//Route parameters
-app.get("/api/users/:id", resolveIndexByUserId, (request, response) => {
-    const {findUserIndex} = request;
-    const findUser = mockUsers[findUserIndex];
-    if (!findUser) return response.sendStatus(404);
-    return response.send(findUser);
 });
 
 
