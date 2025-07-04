2efb1db22fd10d548ea7d22989a78c6f6f8bf531
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-04 17:35:38 +0545

    Implementing constants in utils

---
+++ ExpressJs/Day31/src/utils/constants.mjs
@@ -0,0 +1,9 @@
+export const mockUsers = [
+    {id: 1, username: "kiran", displayName: "Kiran"},
+    {id: 2, username: "ashura", displayName: "Ashura"},
+    {id: 3, username: "unknown", displayName: "Unknown"},
+    {id: 4, username: "ded", displayName: "Dead"},
+    {id: 5, username: "rere", displayName: "Rere"},
+    {id: 6, username: "otokodes", displayName: "Aadim"},
+    {id: 7, username: "yog-anish", displayName: "Anish"},
+];
