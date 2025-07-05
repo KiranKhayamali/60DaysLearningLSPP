2ef7cfe3458c8e94b7d8d439d9d5316cb6b0d4f7
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-05 23:35:10 +0545

    Adding password attributes

--- ExpressJs/Day31/src/utils/constants.mjs
+++ ExpressJs/Day31/src/utils/constants.mjs
@@ -1,9 +1,9 @@
 export const mockUsers = [
-    {id: 1, username: "kiran", displayName: "Kiran"},
-    {id: 2, username: "ashura", displayName: "Ashura"},
-    {id: 3, username: "unknown", displayName: "Unknown"},
-    {id: 4, username: "ded", displayName: "Dead"},
-    {id: 5, username: "rere", displayName: "Rere"},
-    {id: 6, username: "otokodes", displayName: "Aadim"},
-    {id: 7, username: "yog-anish", displayName: "Anish"},
+    {id: 1, username: "kiran", displayName: "Kiran", password: "password"},
+    {id: 2, username: "ashura", displayName: "Ashura", password: "password"},
+    {id: 3, username: "unknown", displayName: "Unknown", password: "password"},
+    {id: 4, username: "ded", displayName: "Dead", password: "password"},
+    {id: 5, username: "rere", displayName: "Rere", password: "password"},
+    {id: 6, username: "otokodes", displayName: "Aadim", password: "password"},
+    {id: 7, username: "yog-anish", displayName: "Anish", password: "password"},
 ];
