c716f1efdf1bdf0fa431106f743bf5d5c7a0317d
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-05 22:56:28 +0545

    Introduction to session

--- ExpressJs/Day31/src/index.mjs
+++ ExpressJs/Day31/src/index.mjs
@@ -1,11 +1,24 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json()); //middleware
 app.use(cookieParser("secret"));
+app.use(
+    session({
+        secret: "this is secret key", //This secret key should be well protected as password
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 60000 * 60, //60000 means 60 sec and its total 1 hrs
+
+        }
+    })
+);
+
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -16,6 +29,9 @@ app.listen(PORT, () => { //localhost:3000
 
 //GET requests
 app.get("/", (request, response) => {
+    console.log(request.session);
+    console.log(request.session.id);
+    request.session.visited = true; //this helps to identify user
     response.cookie("hello", "world", {maxAge: 100000, signed: true});
     response.status(201).send({msg : "Hello World!"});
 });
