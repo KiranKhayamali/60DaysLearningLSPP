7c831e9d06e582b4031c28140a1bee1bcec014c6
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-09 20:13:54 +0545

    Implememting unit testing for send status of 400 when database fails to save user

--- ExpressJs/Day31/src/__tests__/users.spec.js
+++ ExpressJs/Day31/src/__tests__/users.spec.js
@@ -1,6 +1,26 @@
 import validator from "express-validator";
 import { getUserByIdHandler, createUserHandler } from "../handlers/users.mjs";
 import { mockUsers } from "../utils/constants.mjs";
+import * as helpers from "../utils/helpers.mjs";
+import { User } from "../mongoose/schemas/user.mjs";
+
+jest.mock("express-validator", () => ({
+    validationResult: jest.fn(() => ({
+        isEmpty: jest.fn(() => false),
+        array: jest.fn(() => [{ msg: "Invalid field"}]),
+    })),
+    matchedData: jest.fn(() => ({
+        username: "test",
+        displayName: "test_name",
+        password: "password",
+    })),
+}));
+
+jest.mock("../utils/helpers.mjs", () => ({
+    hashPassword: jest.fn((password) => `hashed_${password}`),
+}));
+
+jest.mock("../mongoose/schemas/user.mjs");
 
 const mockRequest = {
     findUserIndex: 3,
@@ -40,4 +60,47 @@ describe("create users", () => {
         expect(mockResponse.send).toHaveBeenCalledWith([{ msg: "Invalid field"}]); 
     });
 
+    it("should return status of 201 and the user created", async () => {
+        jest.spyOn(validator, "validationResult").mockImplementationOnce(() => ({
+            isEmpty: jest.fn(() => true),
+        }));
+
+        const saveMethod = jest.spyOn(User.prototype, "save").mockResolvedValueOnce({
+            id: 1,
+            username: "test",
+            displayName: "test_name",
+            password: "hashed_password",
+        });
+
+        await createUserHandler(mockRequest, mockResponse);
+        expect(validator.matchedData).toHaveBeenCalledWith(mockRequest);
+        expect(helpers.hashPassword).toHaveBeenCalledWith("password");
+        expect(helpers.hashPassword).toHaveReturnedWith("hashed_password");
+        expect(User).toHaveBeenCalledWith({
+            username: "test",
+            displayName: "test_name",
+            password: "hashed_password",
+        });
+
+        expect(saveMethod).toHaveBeenCalled();
+        expect(mockResponse.status).toHaveBeenLastCalledWith(201);
+        expect(mockResponse.send).toHaveBeenLastCalledWith({
+            id: 1,
+            username: "test",
+            displayName: "test_name",
+            password: "hashed_password",
+        });
+    });
+
+    it("send status of 400 when database fails to save user", async () => {
+        jest.spyOn(validator, "validationResult").mockImplementationOnce(() => ({
+            isEmpty: jest.fn(() => true),
+        }));
+
+        const saveMethod = jest.spyOn(User.prototype, "save").mockImplementationOnce(() => Promise.reject("Failed to save user"));
+
+        await createUserHandler(mockRequest, mockResponse);
+        expect(saveMethod).toHaveBeenCalled();
+        expect(mockResponse.sendStatus).toHaveBeenCalledWith(400);
+    });
 });
