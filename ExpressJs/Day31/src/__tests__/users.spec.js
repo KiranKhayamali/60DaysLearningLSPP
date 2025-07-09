fbd9884b58ef72c99b1e74d1d4e35ba39beab624
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-09 20:09:07 +0545

    Implememting unit testing for should get user by id

---
+++ ExpressJs/Day31/src/__tests__/users.spec.js
@@ -0,0 +1,22 @@
+import { getUserByIdHandler, createUserHandler } from "../handlers/users.mjs";
+import { mockUsers } from "../utils/constants.mjs";
+
+const mockRequest = {
+    findUserIndex: 3,
+};
+
+const mockResponse = {
+    sendStatus: jest.fn(),
+    send: jest.fn(),
+    status: jest.fn(() => mockResponse),
+};
+
+describe("get users", () => {
+    it("should get user by id", () => {
+        getUserByIdHandler(mockRequest, mockResponse);
+        expect(mockResponse.send).toHaveBeenCalled();
+        expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[3]);
+        expect(mockResponse.send).toHaveBeenCalledTimes(1);
+    });
+
+});
