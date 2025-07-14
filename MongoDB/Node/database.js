36b8d1ddab892b573d87d0e95237591a740c6b1c
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-14 23:06:32 +0545

    Implementing database online with mongodb atlas

--- MongoDB/Node/database.js
+++ MongoDB/Node/database.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
+let uri = "mongodb+srv://kirankhayamali:vntUn18ZqSktcXsc@cluster0.cqipsvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect("mongodb://localhost:27017/bookstore")
+        // MongoClient.connect("mongodb://localhost:27017/bookstore")
+        MongoClient.connect(uri)
         .then((client) => {
             dbConnection = client.db();
             return cb();
