11ce5b25e2957d710a81d3d214497bb4a3bb5b92
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-24 21:31:48 +0545

    Implementing registration of user

---
+++ MongoDB/Day54/Backend/controllers/authController.js
@@ -0,0 +1,37 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const registerUser = async (req, res) => {
+    const {username, email, password} = req.body;
+
+    try{
+        const existingUser = await User.findOne({ username });
+        if(existingUser) return res.status(400).json({message: "User already exists!"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        const token = jwt.sign({ id: newUser._id}, JWT_SECRET, {expiresIn: "7days"});
+        res.status(201).json({
+            user: {id: newUser._id, username: newUser.username, email: newUser.email},
+            token,
+        });
+    } catch(error){
+        res.status(500).json({message: "Server error!"});
+        console.log(error);
+    }
+};
+
