88690b2960f1b6b0ab9755c3c74199ef1d80294a
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-24 21:32:19 +0545

    Implementing logging in for user

--- MongoDB/Day54/Backend/controllers/authController.js
+++ MongoDB/Day54/Backend/controllers/authController.js
@@ -35,3 +35,24 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    const {username, password} = req.body;
+
+    try{
+        const user = await User.findOne({ username});
+        if(!user) return res.status(404).json({message: "The User does not exists!"});
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(401).json({message: "Invalid Credentials!"});
+
+        const token = jwt.sign({ id: user._id}, JWT_SECRET, {expiresIn: "7days"});
+        res.json({
+            user: {id: user._id, username: user.username, email: user.email},
+            token,
+        });
+    } catch(error){
+        res.status(500).json({message: "Server Error!"});
+    }
+};
+
+module.exports = {registerUser, loginUser};
