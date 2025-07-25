41dc1fa13caa65ce598f06d8f2c2c5c3e2c2e3b6
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-25 17:17:08 +0545

    Fetching all the post

--- MongoDB/Day54/Backend/routes/post.js
+++ MongoDB/Day54/Backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {createPost} = require("../controllers/postController");
+const {createPost, getAllPosts} = require("../controllers/postController");
 
 const router = express.Router();
 
+router.get("/",  getAllPosts);
 router.post("/", protect, createPost);
 
 module.exports = router;
