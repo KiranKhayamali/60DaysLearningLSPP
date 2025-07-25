36beb30bcecc121f95d9eba92bcc04fd84e7ab59
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-25 18:12:11 +0545

    Deleting post using postId

--- MongoDB/Day54/Backend/routes/post.js
+++ MongoDB/Day54/Backend/routes/post.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {createPost, getAllPosts, getPostById, updatePost} = require("../controllers/postController");
+const {createPost, getAllPosts, getPostById, updatePost, deletePost} = require("../controllers/postController");
 
 const router = express.Router();
 
 router.get("/",  getAllPosts);
 router.get("/:id", getPostById);
 router.post("/", protect, createPost);
-router.post("/:id", protect, updatePost);
+router.put("/:id", protect, updatePost);
+router.delete("/:id", protect, deletePost);
+
 
 module.exports = router;
