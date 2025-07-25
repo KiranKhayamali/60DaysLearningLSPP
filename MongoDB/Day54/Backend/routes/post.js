c6323506b42b4f862e4e36405aa7240bef825c17
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-25 17:26:57 +0545

    Fetching the post with postId

--- MongoDB/Day54/Backend/routes/post.js
+++ MongoDB/Day54/Backend/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {createPost, getAllPosts} = require("../controllers/postController");
+const {createPost, getAllPosts, getPostById} = require("../controllers/postController");
 
 const router = express.Router();
 
 router.get("/",  getAllPosts);
+router.get("/:id", getPostById);
 router.post("/", protect, createPost);
 
 module.exports = router;
