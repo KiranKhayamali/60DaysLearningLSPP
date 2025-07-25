9034c6a7fe9edfd21a320a920791757eb4691f58
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-25 16:36:07 +0545

    Implementing JWT token for protecting user data

---
+++ MongoDB/Day54/Backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const protect = async(req, res) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader && authHeader.startsWith("Bearer ")) {
+        const token = authHeader.split(" ")[1];
+        try {
+            const decoded = jwt.verify(token, JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password"); //attach user info
+            next();
+        } catch (error) {
+            return res.status(401).json({message: "Invalid token!"});
+        };
+    } else {
+        return res.status(401).json({message: "Authorization denied, Token missing!"});
+    };
+};
+
+module.exports = protect;
