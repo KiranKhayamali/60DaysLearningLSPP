463f92dd85fe77e6734bdffa8b83c853e71db38b
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-25 17:47:51 +0545

    Updating post using postId

--- MongoDB/Day54/Backend/routes/post.js
+++ MongoDB/Day54/Backend/routes/post.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {createPost, getAllPosts, getPostById} = require("../controllers/postController");
+const {createPost, getAllPosts, getPostById, updatePost} = require("../controllers/postController");
 
 const router = express.Router();
 
 router.get("/",  getAllPosts);
 router.get("/:id", getPostById);
 router.post("/", protect, createPost);
+router.post("/:id", protect, updatePost);
 
 module.exports = router;
