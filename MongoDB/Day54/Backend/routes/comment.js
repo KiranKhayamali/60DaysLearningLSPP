f1435440702209ac9e0daec7efe7f1c90c931487
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-26 17:41:48 +0545

    Deleting comment of the post

--- MongoDB/Day54/Backend/routes/comment.js
+++ MongoDB/Day54/Backend/routes/comment.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const { createComment } = require("../controllers/commentController");
+const { createComment, deleteComment } = require("../controllers/commentController");
 
 const router = express.Router();
 
 router.post("/:postId", protect, createComment);
+router.delete("/:postId", protect, deleteComment);
+
 
 
 module.exports = router;
