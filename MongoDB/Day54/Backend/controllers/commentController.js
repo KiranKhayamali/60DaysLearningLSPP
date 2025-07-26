b71492e47db1d5352520204f80c6022815a8d2ee
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-26 17:41:37 +0545

    Deleting comment of the post

--- MongoDB/Day54/Backend/controllers/commentController.js
+++ MongoDB/Day54/Backend/controllers/commentController.js
@@ -16,5 +16,19 @@ const createComment = async (req, res) => {
     }
 };
 
+const deleteComment = async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if(!comment) return res.status(404).json({message: "Comment Not Found!"});
+        if(comment.user.toString() !== req.user._id.toString()) 
+            return res.status(403).json({message: "Not authorized to delete this comment!"});
 
-module.exports = {createComment};
+        await comment.remove();
+        return res.json({message: "Comment Sucessfully deleted"});
+    } catch (error) {
+        return res.status(500).json({message: "Failed to delete comment!"});
+    }
+};
+
+
+module.exports = {createComment, deleteComment};
