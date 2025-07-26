a3ac853fd6a8933942ae03dd7ceb8c9a3d7b1d33
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-26 18:07:01 +0545

    Updating Comment

--- MongoDB/Day54/Backend/routes/comment.js
+++ MongoDB/Day54/Backend/routes/comment.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const { createComment, deleteComment, getCommentForPost } = require("../controllers/commentController");
+const { createComment, deleteComment, getCommentForPost, updateComment } = require("../controllers/commentController");
 
 const router = express.Router();
 
 router.post("/:postId", protect, createComment);
 router.delete("/:commentId", protect, deleteComment);
-router.get("/posts/:postId", protect, getCommentForPost);
+router.get("/post/:postId", protect, getCommentForPost);
+router.put("/post/:postId", protect, updateComment);
 
 
 
