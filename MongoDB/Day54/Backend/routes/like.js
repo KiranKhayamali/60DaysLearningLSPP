a68f2f536d251ab55dd77f1ad2d3d45238f86c2a
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-27 16:12:22 +0545

    Adding unliking post features

--- MongoDB/Day54/Backend/routes/like.js
+++ MongoDB/Day54/Backend/routes/like.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {likePost} = require("../controllers/likeController");
+const {likePost, unlikePost} = require("../controllers/likeController");
 
 const router = express.Router();
 
 router.post("/:postId", protect, likePost);
+router.delete("/:postId", protect, unlikePost);
 
 
 module.exports = router; 
