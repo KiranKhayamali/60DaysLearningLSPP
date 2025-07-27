c7b2d1e2c99e239fb464014e46ac59ec3d2d4884
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-27 16:32:28 +0545

    Fetching like count

--- MongoDB/Day54/Backend/routes/like.js
+++ MongoDB/Day54/Backend/routes/like.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {likePost, unlikePost} = require("../controllers/likeController");
+const {likePost, unlikePost, getLikesCount} = require("../controllers/likeController");
 
 const router = express.Router();
 
 router.post("/:postId", protect, likePost);
 router.delete("/:postId", protect, unlikePost);
+router.get("/:postId", protect, getLikesCount);
 
 
 module.exports = router; 
