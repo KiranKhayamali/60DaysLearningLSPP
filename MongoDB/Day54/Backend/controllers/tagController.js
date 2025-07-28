6b1f038b3f41713222324fba834c8669904eda74
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-28 21:20:47 +0545

    Fetching posts using multiple tags

--- MongoDB/Day54/Backend/controllers/tagController.js
+++ MongoDB/Day54/Backend/controllers/tagController.js
@@ -10,6 +10,16 @@ const getPostByTag = async (req, res) => {
     };
 };
 
+const getPostByMultipleTags = async (req, res) => {
+    try {
+        const tags = req.query.tags?.split(",").map(tag => tag.trim().toLowerCase());
+        if(!tags||tags.length === 0) return res.status(404).json({message: "Tags Not Found!"});
 
+        const posts = await Post.find({tags: {$in: tags}}).populate("author", "username").sort({created_at: -1});
+        return res.json(posts);
+    } catch (error) {
+        return res.status(500).json({message: "Failed to fetch post by multiple tags", error});
+    };
+};
 
-module.exports = {getPostByTag};
+module.exports = {getPostByTag, getPostByMultipleTags};
