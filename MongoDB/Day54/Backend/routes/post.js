3f0a5b3be4a3913e068eab13739f406808ceb0d8
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-29 19:12:30 +0545

    Fetching post with keyword from title and content

--- MongoDB/Day54/Backend/routes/post.js
+++ MongoDB/Day54/Backend/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {protect} = require("../middlewares/authMiddleware");
-const {createPost, getAllPosts, getPostById, updatePost, deletePost} = require("../controllers/postController");
+const {createPost, getAllPosts, getPostById, updatePost, deletePost, getPostByKeyword} = require("../controllers/postController");
 
 const router = express.Router();
 
 router.get("/",  getAllPosts);
+router.get("/search", getPostByKeyword);
 router.get("/:id", getPostById);
 router.post("/", protect, createPost);
 router.put("/:id", protect, updatePost);
