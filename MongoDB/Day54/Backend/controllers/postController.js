918face022c027cfbe747b711932ad98492ad71a
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-29 19:12:15 +0545

    Fetching post with keyword from title and content

--- MongoDB/Day54/Backend/controllers/postController.js
+++ MongoDB/Day54/Backend/controllers/postController.js
@@ -48,7 +48,6 @@ const updatePost = async (req, res) => {
             {$set: req.body},
             {new: true}
         );
-        console.log("testing");
         return res.json(updatePost);
     } catch (error) {
         return res.status(500).json({message: "Error Updating post!"});
@@ -73,5 +72,20 @@ const deletePost = async (req, res) => {
     };
 };
 
+const getPostByKeyword = async(req, res) => {
+    try {
+        const query = req.query.q;
+        if(!query) return res.status(400).json({message: "Search query is requrired!"});
 
-module.exports = {createPost, getAllPosts, getPostById, updatePost, deletePost};
+        const regex = new RegExp(query, "i") //case-insensitive regex
+        const posts = await Post.find({
+            $or: [{title: regex}, {content: regex}],
+        }).populate("author", "username").sort({created_at: -1});
+        return res.json(posts);
+    } catch(error) {
+        return res.status(500).json({message: "Error while fetching post with keyword", errors: error});
+    }
+};
+
+
+module.exports = {createPost, getAllPosts, getPostById, updatePost, deletePost, getPostByKeyword};
