1672ec6e44bbd7580e196cb6e65957b2a596c55e
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-30 20:38:47 +0545

    Implementing sorting using aggregation pipeline

--- MongoDB/Day54/Backend/controllers/postController.js
+++ MongoDB/Day54/Backend/controllers/postController.js
@@ -126,6 +126,42 @@ const searchPostPaginated = async(req, res) => { //for searching post using both
         
         const skip = (parseInt(page) -1) * parseInt(limit);
 
+        //Sorting post using aggregation pipeline
+        // let posts;
+        // let total;
+        
+        // if(sort === "popular") { 
+        //     const pipline = [
+        //         {
+        //             $addFields: {
+        //                 likeCount: {$size: "$likes"}
+        //             }
+        //         },
+        //         { $sort : {likeCount : -1} },//popularity 
+        //         { $skip: skip },
+        //         { $limit: parseInt(limit) },
+        //         {
+        //             $lookup: {
+        //                 from: "users",
+        //                 localField: "author",
+        //                 foreignField: "_id",
+        //                 as: "author"
+        //             }
+        //         },
+        //         { $unwind: "$author" }
+        //     ];
+        //     posts = await Post.aggregate(pipline);
+        //     total = await Post.countDocuments(filter);
+        // } else {
+        //     posts = await Post.find(filter)
+        //         .populate("author", "username")
+        //         .sort({created_at: -1})
+        //         .skip(skip)
+        //         .limit(parseInt(limit));
+
+        //         total = await Post.countDocuments(filter)
+        // };
+
         let sortOption = {created_at: -1};
         if (sort === "popular") {
             sortOption = {likes: -1}
